627177ba82df878f309c3332b5b582847dce5933
CoderEric04 <94086390+CoderEric04@users.noreply.github.com>
2023-03-26 03:56:02 +0100

    split the data into training and testing

--- src/main/java/etnaivebayes/Main.java
+++ src/main/java/etnaivebayes/Main.java
@@ -1,20 +1,11 @@
 package etnaivebayes;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Reader filereader = new FileReader("MLdata.csv");
-        Iterable<CSVRecord> records=CSVFormat.DEFAULT.parse(filereader);
 
-        for(CSVRecord record:records){
-            String StudentGender = record.get(0);
-            System.out.println(StudentGender);
-        }
+        SplitData sd = new SplitData("MLdata.csv");
+        System.out.println(sd.testingData());
     }
 }
---
+++ src/main/java/etnaivebayes/SplitData.java
@@ -0,0 +1,70 @@
+package etnaivebayes;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitData {
+    String file;
+
+    public SplitData(String file){
+        this.file = file;
+    }
+
+    public List<List<String>> readCSV(String file) throws IOException {
+        Reader filereader = new FileReader(file);
+        CSVParser parser = new CSVParser(filereader, CSVFormat.DEFAULT);
+        List<List<String>> data = new ArrayList<>();
+
+        for (CSVRecord record : parser) {
+            List<String> row = new ArrayList<>();
+            for (String value : record) {
+                row.add(value);
+            }
+            data.add(row);
+        }
+
+        return data;
+    }
+
+    public int numTraining() throws IOException {
+        List<List<String>> data = readCSV("MLdata.csv");
+        int sizeOfTraining = (int) (0.7 * data.size());
+
+        return sizeOfTraining;
+    }
+
+    public int numTesting() throws IOException {
+        List<List<String>> data = readCSV("MLdata.csv");
+        int sizeOfTesting = data.size() - numTraining();
+
+        return sizeOfTesting;
+    }
+
+    public List<List<String>> trainingData() throws IOException {
+        List<List<String>> trainingData = new ArrayList<>();
+
+        for (int i = 0; i < numTraining(); i++) {
+            trainingData.add(new ArrayList<>(readCSV("MLdata.csv").get(i)));
+        }
+
+        return trainingData;
+    }
+
+    public List<List<String>> testingData() throws IOException {
+        List<List<String>> testingData = new ArrayList<>();
+
+        for (int i = 0; i < numTesting(); i++) {
+            testingData.add(new ArrayList<>(readCSV("MLdata.csv").get(i)));
+        }
+
+        return testingData;
+    }
+
+}
